da95d86b8fb0a2f4fe6b983a4dd64de270888796
AlbertLeipzig <albertclemvill@gmail.com>
2024-08-28 10:08:48 +0200

    first commit

---
+++ data/employeesData.js
@@ -0,0 +1,34 @@
+export const employeesData = [
+  {
+    firstName: "Anton",
+    lastName: "Müller",
+    rol: "marketing",
+    mail: "anton.mueller@mail.com",
+    portrait:
+      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fHBvcnRyYWl0fGVufDB8fDB8fHww",
+  },
+  {
+    firstName: "Berta",
+    lastName: "Schäffer",
+    rol: "ceo",
+    mail: "",
+    portrait:
+      "https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBvcnRyYWl0fGVufDB8fDB8fHww",
+  },
+  {
+    firstName: "Cesar",
+    lastName: "Weinberg",
+    rol: "it",
+    mail: "cesar.weinberg@mail.com",
+    portrait:
+      "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fHBvcnRyYWl0fGVufDB8fDB8fHww",
+  },
+  {
+    firstName: "Dora",
+    lastName: "Wagner",
+    rol: "human ressources",
+    mail: "dora.wagner@mail.com",
+    portrait:
+      "https://images.unsplash.com/photo-1506863530036-1efeddceb993?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDZ8fHBvcnRyYWl0fGVufDB8fDB8fHww",
+  },
+];
---
+++ data/productsData.js
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    title: "watch",
+    price: 79,
+    image:
+      "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    title: "Headphones",
+    price: 59,
+    image:
+      "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    title: "Shoes",
+    price: 249,
+    image:
+      "https://images.unsplash.com/photo-1543163521-1bf539c55dd2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    title: "Backpack",
+    price: 69,
+    image:
+      "https://images.unsplash.com/photo-1491637639811-60e2756cc1c7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+  },
+];
